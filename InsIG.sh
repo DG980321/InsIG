c6f5d3dbf96ed353e467d3b085cb6afc23a73b00
Yezer <63272162+DG980321@users.noreply.github.com>
2022-06-28 22:27:25 -0500

    Update InsIG.sh

--- InsIG.sh
+++ InsIG.sh
@@ -209,8 +209,8 @@ fi
 sleep 3
 
 done
-printf " \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 100 secs...\e[0m\n"
-sleep 100
+printf " \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 62 secs...\e[0m\n"
+sleep 62
 #unfollow
 for celeb in $(cat celeb_id); do
 data='{"_uuid":"'$guid'", "_uid":"'$username_id'", "user_id":"'$celeb'", "_csrftoken":"'$var2'"}'
@@ -229,8 +229,8 @@ fi
 
 sleep 3
 done
-printf " \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 100 secs for block prevention...\e[0m\n"
-sleep 100
+printf " \e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;77m Sleeping 62 secs for block prevention...\e[0m\n"
+sleep 62
 
 
 done
